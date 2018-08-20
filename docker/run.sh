e576fd76fb8dfb15e27ff677cc3b4e1f53344d49
Alberto Soragna <alberto.soragna@gmail.com>
2018-08-20 14:56:53 -0700

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -4,19 +4,11 @@
 # @2018 
 
 
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-
-touch $XAUTH
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-
 docker run -it --rm \
-	     --net=host \
-	     -v $XSOCK:$XSOCK:rw \
-	     -v $XAUTH:$XAUTH:rw \
-	     -e XAUTHORITY=${XAUTH} \
-             -e DISPLAY=$DISPLAY \
-             -e QT_GRAPHICSSYSTEM=native \
-	     --name ros \
-	     ros2_docker \
-	     bash
+	  --net=host \
+	  --privileged \
+	  --volume=/tmp/.X11-unix:/tmp/.X11-unix \
+	  --device=/dev/dri:/dev/dri \
+	  --env="DISPLAY" \
+	  ros2_docker \
+	  bash
