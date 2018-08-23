83726d7ccc37a6d826e42135bbef6f1e2cb67e94
Alberto Soragna <alberto.soragna@gmail.com>
2018-08-23 14:00:56 -0700

    Add files via upload

--- external_build_tools/hello_ros.cpp
+++ external_build_tools/hello_ros.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include "rclcpp/rclcpp.hpp"
 
-int main(int argc, char* argv[])
-    {
-      std::cout<<"Hello World!"<<std::endl;
-      return 0;
-    }
+int main(int argc, char ** argv)
+{
+
+	rclcpp::init(argc, argv);
+
+	auto node = rclcpp::Node::make_shared("test_node");
+
+	RCLCPP_INFO(node->get_logger(), "Hello World from this ROS2 node!");
+
+	return 0;
+}
     
