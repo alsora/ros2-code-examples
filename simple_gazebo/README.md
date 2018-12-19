c15e1349fdc9cda6b4b2c69c8dbcfbf23498aaec
alberto <asoragna@irobot.com>
2018-12-19 11:19:40 -0800

    added gazebo instructions; cleaned logger

---
+++ simple_gazebo/README.md
@@ -0,0 +1,52 @@
+# simple_gazebo
+
+ROS2 Crystal Clemmys introduces support for the 3D Simulation and Visualization tool Gazebo 9.
+
+## Install
+
+Install Gazebo
+[More detailed instructions](http://gazebosim.org/tutorials?cat=guided_b&tut=guided_b1)
+
+    $ wget https://bitbucket.org/osrf/release-tools/raw/default/one-line-installations/gazebo.sh
+    $ chmod +x gazebo.sh
+    $ ./gazebo.sh
+
+Source Gazebo setup file (NOT MANDATORY)
+
+    $ source /usr/share/gazebo/setup.sh
+
+Test if Gazebo is working
+
+    $ gazebo
+
+
+Create a workspace for gazebo_ros_pkgs
+
+    $ mkdir -p ~/gazebo_ws/src
+    $ cd ~/gazebo_ws/src
+    $ git clone -b ros2 https://github.com/ros-simulation/gazebo_ros_pkgs
+    $ git clone -b ros2 https://github.com/ros-perception/vision_opencv
+    $ git clone -b ros2 https://github.com/ros-perception/image_common
+
+Install other dependencies
+
+    $ cd ..
+    $ rosdep install --from-paths src ~/ros2_ws/src --ignore-src --rosdistro crystal -y --skip-keys "console_bridge fastcdr fastrtps libopensplice67 libopensplice69 rti-connext-dds-5.3.1 urdfdom_headers"
+
+Build the workspace and source it
+
+    $ colcon build
+    $ source install/local_setup.sh
+
+Now everything should be ready.
+You can run a Gazebo world file, as the example provided in the gazebo plugins package
+
+    $ gazebo --verbose install/gazebo_plugins/share/gazebo_plugins/worlds/gazebo_ros_diff_drive_demo.world
+
+Now you should be able to see the ROS2 topics used by this example
+
+    $ ros2 topic list
+
+For example, to send a velocity command to the robot
+
+    $ ros2 topic pub /demo/cmd_demo geometry_msgs/Twist '{linear: {x: 1.0}}' -1
--- simple_logger/logger_main.cpp
+++ simple_logger/logger_main.cpp
@@ -9,41 +9,39 @@
 int main(int argc, char ** argv)
 {
 
-  // initialize ros2 and createa a node
-  rclcpp::init(argc, argv);
-  rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("simple_node");
+    // initialize ros2 and create a node
+    rclcpp::init(argc, argv);
+    rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("simple_node");
 
-  std::cout<<"Printing some messages with default verbosity level INFO"<<std::endl;
+    std::cout<<"Printing some messages with default verbosity level INFO"<<std::endl;
 
-  // print some log messages
-  RCLCPP_DEBUG(node->get_logger(), "This is a RCLCPP_DEBUG message");
-  RCLCPP_INFO(node->get_logger(), "This is a RCLCPP_INFO message");
-  RCLCPP_WARN(node->get_logger(), "This is a RCLCPP_WARN message");
-  RCLCPP_ERROR(node->get_logger(), "This is a RCLCPP_ERROR message");
+    // print some log messages
+    RCLCPP_DEBUG(node->get_logger(), "This is a RCLCPP_DEBUG message");
+    RCLCPP_INFO(node->get_logger(), "This is a RCLCPP_INFO message");
+    RCLCPP_WARN(node->get_logger(), "This is a RCLCPP_WARN message");
+    RCLCPP_ERROR(node->get_logger(), "This is a RCLCPP_ERROR message");
 
-  // change the logger verbosity level at runtime
-  auto ret = rcutils_logging_set_logger_level(node->get_logger().get_name(), RCUTILS_LOG_SEVERITY_WARN);
-  if (ret != RCUTILS_RET_OK) {
-    RCLCPP_ERROR(node->get_logger(), "Error setting severity: %s", rcutils_get_error_string());
-    rcutils_reset_error();
-  }
-  std::cout<<"Changing verbosity level to WARN"<<std::endl;
+    // change the logger verbosity level at runtime
+    std::cout<<"Changing verbosity level to WARN"<<std::endl;
+    auto ret = rcutils_logging_set_logger_level(node->get_logger().get_name(), RCUTILS_LOG_SEVERITY_WARN);
+    if (ret != RCUTILS_RET_OK) {
+        RCLCPP_ERROR(node->get_logger(), "Error setting severity: %s", rcutils_get_error_string());
+        rcutils_reset_error();
+    }
 
-  // print again some log messages to see the differences
-  RCLCPP_DEBUG(node->get_logger(), "This is a RCLCPP_DEBUG message");
-  RCLCPP_INFO(node->get_logger(), "This is a RCLCPP_INFO message");
-  RCLCPP_WARN(node->get_logger(), "This is a RCLCPP_WARN message");
-  RCLCPP_ERROR(node->get_logger(), "This is a RCLCPP_ERROR message");
+    // print again some log messages to see the differences
+    RCLCPP_DEBUG(node->get_logger(), "This is a RCLCPP_DEBUG message");
+    RCLCPP_INFO(node->get_logger(), "This is a RCLCPP_INFO message");
+    RCLCPP_WARN(node->get_logger(), "This is a RCLCPP_WARN message");
+    RCLCPP_ERROR(node->get_logger(), "This is a RCLCPP_ERROR message");
 
+    // create a logger not related to any node
+    rclcpp::Logger logger = rclcpp::get_logger("my_logger");
 
-  // create a logger not related to any node
-  rclcpp::Logger logger = rclcpp::get_logger("my_logger");
-
-  RCLCPP_WARN(logger, "This is a RCLCPP_WARN message");
-
+    RCLCPP_WARN(logger, "This is a RCLCPP_WARN message from a logger not related to any node");
 
 
-  rclcpp::shutdown();
-  node = nullptr;
-  return 0;
+    rclcpp::shutdown();
+    node = nullptr;
+    return 0;
 }
