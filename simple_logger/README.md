62f4cb1a4ad3c7ff9f7e259bc070b0ed880bec09
alberto <asoragna@irobot.com>
2019-03-13 11:12:39 +0000

    updated simple_logger

--- simple_logger/README.md
+++ simple_logger/README.md
@@ -20,12 +20,25 @@ It is possible to change the verbosity level of each logger at runtime.
 Moreover, we show how to create a logger that is not connected to any node.
 
 
-#### ROSOUT TOPIC
+#### rosout Topic
 
 A new feature introduced in January 2019, is the possibility of publishing all the logs to a `rosout` topic.
 You can try it by buildin the master branch of ROS2 or any distribution successive to Crystal.
+
+The interface for `Log` messages is defined [here](https://github.com/ros2/rcl_interfaces/blob/master/rcl_interfaces/msg/Log.msg).
+
 
 This feature is automatically enabled.
 You can disable it through a command line argument
 
     ros2 run simple_logger logger_main __log_disable_rosout:=true
+
+
+
+
+**NOTE:** This feature will only work for loggers associated with a node. According to the ROS documentation:
+
+```
+Because of the way ROS 2 associates DDS concepts with ROS Nodes, there didn’t seem to be a clean way to setup a general topic on a process that wasn’t associated with an existing Node.
+Since a process can also contain multiple ROS Nodes, we didn’t think it would be good design to just pick one to have everything published to.
+```
--- simple_logger/logger_main.cpp
+++ simple_logger/logger_main.cpp
@@ -6,6 +6,8 @@
 #include <memory>
 #include <string>
 
+using namespace std::chrono_literals;
+
 int main(int argc, char ** argv)
 {
 
@@ -13,14 +15,22 @@ int main(int argc, char ** argv)
     rclcpp::init(argc, argv);
     rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("simple_node");
 
-    std::cout<<"Printing some messages with default verbosity level INFO"<<std::endl;
+    // wait some time for discovery
+    std::this_thread::sleep_for(std::chrono::seconds(1s));
+
+    std::cout<<"Logging some messages with default verbosity level INFO"<<std::endl;
 
     // print some log messages
+    std::cout<<"Logging a DEBUG message"<<std::endl;
     RCLCPP_DEBUG(node->get_logger(), "This is a RCLCPP_DEBUG message");
+    std::cout<<"Logging a INFO message"<<std::endl;
     RCLCPP_INFO(node->get_logger(), "This is a RCLCPP_INFO message");
+    std::cout<<"Logging a WARN message"<<std::endl;
     RCLCPP_WARN(node->get_logger(), "This is a RCLCPP_WARN message");
+    std::cout<<"Logging a ERROR message"<<std::endl;
     RCLCPP_ERROR(node->get_logger(), "This is a RCLCPP_ERROR message");
 
+    std::this_thread::sleep_for(std::chrono::seconds(1s));
     // change the logger verbosity level at runtime
     std::cout<<"Changing verbosity level to WARN"<<std::endl;
     auto ret = rcutils_logging_set_logger_level(node->get_logger().get_name(), RCUTILS_LOG_SEVERITY_WARN);
@@ -29,17 +39,26 @@ int main(int argc, char ** argv)
         rcutils_reset_error();
     }
 
+    std::cout<<"Logging some messages with custom verbosity level WARN"<<std::endl;
+
     // print again some log messages to see the differences
+    std::cout<<"Logging a DEBUG message"<<std::endl;
     RCLCPP_DEBUG(node->get_logger(), "This is a RCLCPP_DEBUG message");
+    std::cout<<"Logging a INFO message"<<std::endl;
     RCLCPP_INFO(node->get_logger(), "This is a RCLCPP_INFO message");
+    std::cout<<"Logging a WARN message"<<std::endl;
     RCLCPP_WARN(node->get_logger(), "This is a RCLCPP_WARN message");
+    std::cout<<"Logging a ERROR message"<<std::endl;
     RCLCPP_ERROR(node->get_logger(), "This is a RCLCPP_ERROR message");
+
+    std::this_thread::sleep_for(std::chrono::seconds(1s));
 
     // create a logger not related to any node
     rclcpp::Logger logger = rclcpp::get_logger("my_logger");
 
     RCLCPP_WARN(logger, "This is a RCLCPP_WARN message from a logger not related to any node");
 
+    std::this_thread::sleep_for(std::chrono::seconds(1s));
 
     rclcpp::shutdown();
     node = nullptr;
