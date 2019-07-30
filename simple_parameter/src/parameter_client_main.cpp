b24c970ef8e78cc519752ab8cee4af67d66f5494
Soragna, Alberto <alberto.soragna@gmail.com>
2019-07-30 15:13:03 +0100

    make node spinning to receive param callbacks

--- simple_parameter/src/parameter_client_main.cpp
+++ simple_parameter/src/parameter_client_main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char ** argv)
   std::this_thread::sleep_for(std::chrono::seconds(1));
   node->set_correct_parameters();
 
-  std::this_thread::sleep_for(std::chrono::seconds(3));
+  rclcpp::spin(node);
 
   rclcpp::shutdown();
   return 0;
